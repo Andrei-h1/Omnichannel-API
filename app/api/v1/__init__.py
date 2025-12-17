3607393e3f156172a9c183ddaaddd3c2ef5170be
Andrei <dev@h1drogen.com>
2025-12-17 13:44:41 -0300

    add __init__.py from api/v1

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -7,6 +7,10 @@ from .webhooks_chatwoot import router as webhooks_chatwoot_router
 # File Proxy
 from .file_proxy import router as file_proxy_router
 
+# BOT Endpoints
+from .customers import router as customers_router
+from .bot_sessions import router as bot_sessions_router
+
 api_router = APIRouter()
 
 # ========== Webhooks (funcionam agora) ==========
@@ -15,6 +19,10 @@ api_router.include_router(webhooks_chatwoot_router, prefix="/webhooks/chatwoot",
 
 # ========== File Proxy ==========================
 api_router.include_router(file_proxy_router, prefix="/files", tags=["proxy"])
+
+# ========== BOT Endpoints =======================
+api_router.include_router(customers_router, prefix="/customer", tags=["customers"])
+api_router.include_router(bot_sessions_router, prefix="/bot_sessions", tags=["bot_sessions"])
 
 # ========== CRUDs (ativaremos depois) ===========
 # from .vendors import router as vendors_router
